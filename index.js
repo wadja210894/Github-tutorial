f5601e37570d0b7918a42b9d21172833276bc062
Vadym_Hermaniuk <54590021+wadja210894@users.noreply.github.com>
2023-02-22 17:09:06 +0200

    add

--- index.js
+++ index.js
@@ -1,25 +1,12 @@
-// Example 5 - Toggle
-// Напишіть клас Toggle який приймає об'єкт налаштувань {isOpen: boolean} і оголошує одну властивість on - стан вкл/викл (true/false).
-//За замовчуванням значення властивості on повинно бути false.
+const technologies = ["HTML", "CSS", "JavaScript", "React", "Node"];
+const list = document.querySelector(".list");
 
-class Toggle {
-  constructor({isOpen} = false) { 
-    this.on = isOpen || false;
-  }
+const markup = technologies
+  .map((technology) => `<li class="list-item">${technology}</li>`)
+  .join("");
 
-  toggle =  () => this.on = !this.on;
-}
+// Check the console, you'll see a single string with HTML tags
+console.log(markup);
 
-const firstToggle = new Toggle({ isOpen: true });
-console.group('firstToggle');
-console.log(firstToggle.on);
-firstToggle.toggle();
-console.log(firstToggle.on);
-console.groupEnd('firstToggle');
-
-const secondToggle = new Toggle();
-console.group('secondToggle');
-console.log(secondToggle.on);
-secondToggle.toggle();
-console.log(secondToggle.on);
-console.groupEnd('secondToggle');
+// Adding all the markup in one operation
+list.innerHTML = markup;
